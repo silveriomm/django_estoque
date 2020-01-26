f653349d7ff4bc7e674ea571782471bc22218ad2
silveriomm <silveriomm@bol.com.br>
2020-01-26 19:36:38 -0200

    concluido produto form - aula-13

--- projeto/settings.py
+++ projeto/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Others Apps
+    'widget_tweaks',
     # My Apps
     'projeto.core',
     'projeto.produto',
