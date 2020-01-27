2415bd08710d4f4fe94e5837edc5f621589efda0
silveriomm <silveriomm@bol.com.br>
2020-01-27 21:20:24 -0200

    estoque saida detail - aula24

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -22,8 +22,6 @@ class Estoque(TimeStampedModel):
     def __str__(self):
         return '{} - {} - {}'.format(self.pk, self.nf, self.created.strftime('%d-%m-%Y'))
 
-    def get_absolute_url(self):
-        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk':self.pk})
 
     def nf_formated(self):
         return str(self.nf).zfill(3)
@@ -38,6 +36,9 @@ class EstoqueEntrada(Estoque):
         verbose_name='estoque_entrada'
         verbose_name_plural='estoque_entrada'
 
+    def get_absolute_url(self):
+        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk':self.pk})
+
 
 class EstoqueSaida(Estoque):
 
@@ -47,6 +48,9 @@ class EstoqueSaida(Estoque):
         proxy=True
         verbose_name='estoque_saida'
         verbose_name_plural='estoque_saida'
+
+    def get_absolute_url(self):
+        return reverse_lazy('estoque:estoque_saida_detail', kwargs={'pk':self.pk})
 
 
 class EstoqueItens(models.Model):
