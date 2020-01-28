9d38c8e3c2e0e4e07745e00f2581f4d4f0e98f8a
silveriomm <silveriomm@bol.com.br>
2020-01-28 08:49:30 -0200

    tirando input movimento do form - aula27

--- projeto/estoque/forms.py
+++ projeto/estoque/forms.py
@@ -6,11 +6,11 @@ class EstoqueForm(forms.ModelForm):
 
     class Meta:
         model = Estoque
-        fields = '__all__'
+        fields = ('funcionario', 'nf')
 
 
 class EstoqueItensForm(forms.ModelForm):
-    
+
     class Meta:
         model = EstoqueItens
         fields = '__all__'
