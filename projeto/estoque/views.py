beb71d11de9a0e0922e02ec1d84fbe95c5eee794
silveriomm <silveriomm@bol.com.br>
2020-01-28 13:19:10 -0200

    refatorando estoque detail - aula30

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -16,9 +16,12 @@ def estoque_entrada_list(request):
     return render(request, template_name, context)
 
 def estoque_entrada_detail(request, pk):
-    template_name = 'estoque_entrada_detail.html'
+    template_name = 'estoque_detail.html'
     obj = EstoqueEntrada.objects.get(pk=pk)
-    context = {'object':obj}
+    context = {
+        'object':obj,
+        'url_list': 'estoque:estoque_entrada_list'
+    }
     return render(request, template_name, context)
 
 def dar_baixa_estoque(form):
@@ -82,9 +85,12 @@ def estoque_saida_list(request):
     return render(request, template_name, context)
 
 def estoque_saida_detail(request, pk):
-    template_name = 'estoque_saida_detail.html'
+    template_name = 'estoque_detail.html'
     obj = EstoqueSaida.objects.get(pk=pk)
-    context = {'object':obj}
+    context = {
+        'object':obj,
+        'url_list': 'estoque:estoque_saida_list'
+    }
     return render(request, template_name, context)
 
 def estoque_saida_add(request):
